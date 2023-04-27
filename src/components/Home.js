ac76f0d1d748edd14af846e162e52b8acfdb7808
Ryan Rojas <landoslair@gmail.com>
2023-04-27 16:24:58 -0400

    Added NavLinks to Home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import ArtistIntro from './HomeComponents/ArtistIntro';
 import ArtistEarlyYears from './HomeComponents/ArtistEarlyYears';
 import FirstAlbum from './HomeComponents/FirstAlbum';
@@ -12,29 +13,60 @@ import BillNye from './HomeComponents/BillNye';
 import SixthAlbum from './HomeComponents/SixthAlbum';
 import SeventhAlbum from './HomeComponents/SeventhAlbum';
 import ArtistOutro from './HomeComponents/ArtistOutro';
+import HomeNavBar from './HomeComponents/HomeNavBar';
 
 function Home() {
   return (
-    <div>
-      <div className = 'artist-name'>
+    <div className='home-page'>
+      <div className='artist-name'>
         <h1>
-        Tyler the Creator
+          Tyler the Creator
         </h1>
       </div>
+      <HomeNavBar />
 
-      <ArtistIntro />
-      <ArtistEarlyYears />
-      <FirstAlbum />
-      <YonkersSingle />
-      <SecondAlbum />
-      <LoiterSquad />
-      <ThirdAlbum />
-      <FourthAlbum />
-      <BillNye />
-      <FifthAlbum />
-      <SixthAlbum />
-      <SeventhAlbum />
-      <ArtistOutro />
+      <Switch>
+        <Route path="/artist-intro">
+          <ArtistIntro />
+        </Route>
+        <Route path="/early-years">
+          <ArtistEarlyYears />
+        </Route>
+        <Route path="/first-album">
+          <FirstAlbum />
+        </Route>
+        <Route path="/yonkers">
+          <YonkersSingle />
+        </Route>
+        <Route path="/second-album">
+          <SecondAlbum />
+        </Route>
+        <Route path="/loiter-squad">
+          <LoiterSquad />
+        </Route>
+        <Route path="/third-album">
+          <ThirdAlbum />
+        </Route>
+        <Route path="/fourth-album">
+          <FourthAlbum />
+        </Route>
+        <Route path="/bill-nye">
+          <BillNye />
+        </Route>
+        <Route path="/fifth-album">
+          <FifthAlbum />
+        </Route>
+        <Route path="/sixth-album">
+          <SixthAlbum />
+        </Route>
+        <Route path="/seventh-album">
+          <SeventhAlbum />
+        </Route>
+        <Route path="/outro">
+          <ArtistOutro />
+        </Route>
+      </Switch>
+      <br /><br /><br /><br /><br />
     </div>
   );
 }
