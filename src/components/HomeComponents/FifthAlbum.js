f672bfea39543d1a8e920aa06417f5780f3baa14
Andrew OBrien <aobrienaf@gmail.com>
2023-04-27 16:18:13 -0500

    fifth album link

--- src/components/HomeComponents/FifthAlbum.js
+++ src/components/HomeComponents/FifthAlbum.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import YoutubeEmbed from '../YoutubeEmbed';
 
 function FifthAlbum () {
     return (
@@ -7,8 +8,8 @@ function FifthAlbum () {
                 On July 6, 2017, he announced the title, tracklist and release date of his fifth album, Flower Boy, which was released on July 21, 2017. (album art here). 
                 This changed everything for Tyler as it was such a huge success and so well received by critics that it got him his second Grammy nomination. 
             </h3>
+            <YoutubeEmbed embedID='FcBfSwxOVUM'/>
         </div>
-
     );
 }
 export default FifthAlbum;
