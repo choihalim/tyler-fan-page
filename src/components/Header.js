a5c16663deff529cf9ba5c96cda7ee4467e4e1cf
Ryan Rojas <landoslair@gmail.com>
2023-04-27 10:16:38 -0400

    moved Dark Mode button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import NavBar from './NavBar'
 
 
 
 function Header() {
 
+    const [theme, setTheme] = useState('light');
+
+    const toggleTheme = () => {
+        if (theme === 'light') {
+            setTheme('dark');
+        } else {
+            setTheme('light');
+        }
+    };
+
+    useEffect(() => {
+        // localStorage.setItem('theme', theme);
+        document.body.className = theme;
+    }, [theme]);
+
     return (
         <div>
-            <img className='banner-img' src="https://pbs.twimg.com/media/EVM9MCMUwAAcxla.jpg:large" alt='Tyler Banner' /> <br />
+            <div className='dark-mode-button'>
+                <button  onClick={toggleTheme}>{theme === 'light' ? "Dark Mode" : "Creative Mode"}</button>
+            </div>
+                
+            
+            <div>
+                <img className='banner-img' src="https://pbs.twimg.com/media/EVM9MCMUwAAcxla.jpg:large" alt='Tyler Banner' /> <br />
+            </div>
 
             <NavBar />
 
