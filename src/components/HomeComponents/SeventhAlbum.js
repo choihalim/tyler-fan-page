d6c1028fff150a7d393ce3de879f58967a13bf62
Andrew OBrien <aobrienaf@gmail.com>
2023-04-27 16:31:42 -0500

    seventh album link

--- src/components/HomeComponents/SeventhAlbum.js
+++ src/components/HomeComponents/SeventhAlbum.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import YoutubeEmbed from '../YoutubeEmbed';
 
 function SeventhAlbum () {
     return (
@@ -7,9 +8,9 @@ function SeventhAlbum () {
                 For his seventh studio album, Call Me If You Get Lost, Okonma placed billboards in major cities across the world containing a phone number that when called, played a recorded conversation between Okonma and his mother. 
                 That recording is included in the album as "Momma Talk". Soon after the billboards were spotted, a website of the same name was discovered. 
                 The album's lead single, "Lumberjack", was released on June 16 and upon release, it received widespread critical acclaim and debuted at number one on the US Billboard 200, becoming Okonma's second number-one album in the United States. 
-                This success kept going as this album also netted him his Grammy at the 64th Grammy Awards. 
-                (album art here)
+                This success kept going as this album also netted him his Grammy at the 64th Grammy Awards.
             </h3>
+            <YoutubeEmbed embedID='Mdt4bsdUMdU'/>
         </div>
 
     );
